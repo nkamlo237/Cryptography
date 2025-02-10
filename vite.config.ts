c382a62496551bb123e4a6167686acd661306965
nkamlo <nkamlofouyet07@gmail.com>
2025-02-10 11:09:21 +0100

    git/github initialization

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.JPG"],
-  base: "https://nkamlo237.github.io/cryptography/",
+  base: "/cryptography/",
   build: {
     outDir: "dist",
     rollupOptions: {
