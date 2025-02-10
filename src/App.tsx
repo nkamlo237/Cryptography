7aef165d71e9663f2315b5322a306662c54d9507
nkamlo <nkamlofouyet07@gmail.com>
2025-02-10 11:20:12 +0100

    git/github initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="h-screen w-full bg-black flex items-center justify-center">
-      <span className="text-white font-bold text-lg">Hello, Welcome</span>
+      <span className="text-white font-bold text-lg">Hello, Welcome!!!!</span>
     </div>
   );
 }
