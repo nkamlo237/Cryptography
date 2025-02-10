5edf16d1962f08c31d7f4e0214df7258547e4ec7
nkamlo <nkamlofouyet07@gmail.com>
2025-02-10 10:10:05 +0100

    git/github initialization

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-full bg-black flex items-center justify-center">
+      <span className="text-white font-bold text-lg">Hello, Welcome</span>
+    </div>
+  );
+}
+
+export default App;
