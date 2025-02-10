0c4d98d4e6b9f9686ccc500342c714d9e497bfd7
nkamlo <nkamlofouyet07@gmail.com>
2025-02-10 10:44:42 +0100

    git/github initialization

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.JPG"],
-  base: "/pages/dist",
+  base: "/dist",
 });
